818c64288a4b7cb18bbd9fe1339cbfc04accd468
ml.garcia.lugones <ml.garcia.lugones@gmail.com>
2021-12-12 10:02:13 -0500

    Removed node_module folder

--- node_modules/ng-select2-component/public_api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/select2.component';
-export * from './lib/select2-hint.component';
-export * from './lib/select2-label.component';
-export * from './lib/select2-utils';
-export * from './lib/select2.module';
